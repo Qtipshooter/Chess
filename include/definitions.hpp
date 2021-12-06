71e32d187d58f9cc262eaa5c44bb4b10eeea8f51
Qtipshooter <61977273+Qtipshooter@users.noreply.github.com>
2021-12-06 14:56:18 -0500

    Added comments and include guard

--- include/definitions.hpp
+++ include/definitions.hpp
@@ -1,3 +1,11 @@
+//definitions.hpp
+//Quinton Graham
+//Holds definitions for the project
+
+#ifndef CHESS_DEFINITIONS_HPP
+#define CHESS_DEFINITIONS_HPP
+
+//Piece definitions
 #define PAWN        0x01
 #define ROOK        0x02
 #define BISHOP      0x03
@@ -6,7 +14,11 @@
 #define KING        0x06
 #define EMPTY_PIECE 0x00
 
+//Definition for flag of if piece was moved (Used for castling and pawns)
 #define MOVED       0x10
 
+//Definitions for player 1 and player 2
 #define PLAYER_1    0x20
 #define PLAYER_2    0x40
+
+#endif //CHESS_DEFINITIONS_HPP
