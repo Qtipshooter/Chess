9f5c58b4142ce5c10edf59f8a5c929f34aa1b456
Quinton Graham <61977273+Qtipshooter@users.noreply.github.com>
2021-12-06 13:18:34 -0500

    Added piece definitions

---
+++ include/definitions.hpp
@@ -0,0 +1,12 @@
+#define PAWN        0x01
+#define ROOK        0x02
+#define BISHOP      0x03
+#define KNIGHT      0x04
+#define QUEEN       0x05
+#define KING        0x06
+#define EMPTY_PIECE 0x00
+
+#define MOVED       0x10
+
+#define PLAYER_1    0x20
+#define PLAYER_2    0x40
