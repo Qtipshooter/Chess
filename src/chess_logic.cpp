aaa463cd0239fa0886901c9ac1cc854b3a6677ad
Quinton Graham <61977273+Qtipshooter@users.noreply.github.com>
2021-12-09 15:37:30 -0500

    Added functions to check direction of movee

--- src/chess_logic.cpp
+++ src/chess_logic.cpp
@@ -45,6 +45,48 @@ bool is_valid_move(const unsigned char board[8][8], int current_x, int current_y
     return true;
 }
 
+//Function to see if a position is directly horizontal
+bool is_horizontal(int current_x, int current_y, int new_x, int new_y)
+{
+    if(current_y == new_y)
+    {
+        return true;
+    }
+    return false;
+}
+
+//Function to see if a position is directly vertical
+bool is_vertical(int current_x, int current_y, int new_x, int new_y)
+{
+    if(current_x == new_x)
+    {
+        return true;
+    }
+    return false;
+}
+
+//Function to see if a position is directly diagonal
+bool is_diagonal(int current_x, int current_y, int new_x, int new_y)
+{
+    int dif_x = current_x - new_x;
+    int dif_y = current_y - new_y;
+    
+    if(dif_x < 0)
+    {
+        dif_x = dif_x - dif_x - dif_x;
+    }
+    if(dif_y < 0)
+    {
+        dif_y = dif_y - dif_y - dif_y;
+    }
+
+    if(dif_x == dif_y)
+    {
+        return true;
+    }
+    return false;
+}
+
 //Function to check if a player is in check
 bool is_check(const unsigned char board[8][8], unsigned char player)
 {
@@ -60,5 +102,6 @@ bool is_checkmate(const unsigned char board[8][8], unsigned char player)
 //Function that returns a new proposed board layout
 unsigned char*** get_proposed_board(const unsigned char board[8][8], int current_x, int current_y, int new_x, int new_y)
 {
+
     //return
 }
