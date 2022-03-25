0f3a9ffdba93474a7b388a7cd1276e42c7c2cc1f
Quinton Graham <61977273+Qtipshooter@users.noreply.github.com>
2022-03-25 11:12:41 -0400

    Added WindowHandle class outline

---
+++ include/WindowHandle.hpp
@@ -0,0 +1,28 @@
+//WindowHandle.hpp
+//Quinton Graham
+//The Class definition of a Window Handle for this project
+
+#ifndef WINDOWHANDLE_HPP
+#define WINDOWHANDLE_HPP
+
+#include <SDL2/SDL.h>
+
+//class definition
+class WindowHandle
+{
+    //Class Variables
+    SDL_Window * window;
+    SDL_Renderer * renderer;
+    SDL_Point screen_size;
+    
+    
+    //Constructors
+    //Destructor
+    //Copy Constructor
+    //Copy Assignement Operator
+    //Move Constructor
+    //Move Assignement Operator
+    
+};
+
+#endif // WINDOWHANDLE_HPP
---
+++ src/WindowHandle.cpp
@@ -0,0 +1,6 @@
+//WindowHandle.cpp
+//Quinton Graham
+//Implements the window handle class in this project
+
+#include "WindowHandle.hpp"
+#include <SDL2/SDL.h>
